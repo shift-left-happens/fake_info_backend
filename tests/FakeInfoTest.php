d676c27486d7f0f6df71f41caaaca16eb110ce96
jarrald <novijgod@yahoo.dk>
2026-03-24 15:46:35 +0100

    add data provider

--- tests/FakeInfoTest.php
+++ tests/FakeInfoTest.php
@@ -6,62 +6,114 @@
  * A data provider allows a single test method to run multiple times with
  * different inputs. For example:
  *
- *   testFieldExists($field)
- *   → runs for 'firstName', 'lastName', 'gender'
-*/
+ * testFieldExists($field)
+ * → runs for 'firstName', 'lastName', 'gender'
+ */
 
-<?php
+ <?php
 
-use PHPUnit\Framework\TestCase;
+    use PHPUnit\Framework\Attributes\DataProvider;
+    use PHPUnit\Framework\TestCase;
 
-require_once __DIR__ . '/../src/FakeInfo.php';
+    require_once __DIR__ . '/../src/FakeInfo.php';
 
-class FakeInfoTest extends TestCase
-{
-    public function testFirstNameExists()
+    class FakeInfoTest extends TestCase
     {
-        $fakeInfo = new FakeInfo();
-        $result = $fakeInfo->getFullNameAndGender();
 
-        $this->assertArrayHasKey('firstName', $result);
+        public static function getTestData(): array
+        {
+            return [
+                // getFullNameAndGender fields
+                ['firstName', 'getFullNameAndGender'],
+                ['lastName', 'getFullNameAndGender'],
+                ['gender', 'getFullNameAndGender'],
+
+                // getFullNameGenderAndBirthDate fields
+                ['firstName', 'getFullNameGenderAndBirthDate'],
+                ['lastName', 'getFullNameGenderAndBirthDate'],
+                ['gender', 'getFullNameGenderAndBirthDate'],
+                ['birthDate', 'getFullNameGenderAndBirthDate'],
+
+                // getCprFullNameAndGender fields
+                ['CPR', 'getCprFullNameAndGender'],
+                ['firstName', 'getCprFullNameAndGender'],
+                ['lastName', 'getCprFullNameAndGender'],
+                ['gender', 'getCprFullNameAndGender'],
+
+                // getCprFullNameGenderAndBirthDate fields
+                ['CPR', 'getCprFullNameGenderAndBirthDate'],
+                ['firstName', 'getCprFullNameGenderAndBirthDate'],
+                ['lastName', 'getCprFullNameGenderAndBirthDate'],
+                ['gender', 'getCprFullNameGenderAndBirthDate'],
+                ['birthDate', 'getCprFullNameGenderAndBirthDate'],
+
+                // getFakePerson fields
+                ['CPR', 'getFakePerson'],
+                ['firstName', 'getFakePerson'],
+                ['lastName', 'getFakePerson'],
+                ['gender', 'getFakePerson'],
+                ['birthDate', 'getFakePerson'],
+                ['phoneNumber', 'getFakePerson'],
+            ];
+        }
+        #[DataProvider('getTestData')]
+        public function testExists($fieldname, $method)
+        {
+            $fakeInfo = new FakeInfo();
+            $result = $fakeInfo->$method();
+
+            $this->assertArrayHasKey($fieldname, $result);
+        }
+
+        #[DataProvider('getTestData')]
+        public function testIsString($fieldname, $method)
+        {
+            $fakeInfo = new FakeInfo();
+            $result = $fakeInfo->$method();
+
+            $this->assertIsString($result[$fieldname]);
+        }
+
+        #[DataProvider('getTestData')]
+        public function testIsNotEmpty($fieldname, $method)
+        {
+            $fakeInfo = new FakeInfo();
+            $result = $fakeInfo->$method();
+
+            $this->assertNotEmpty($result[$fieldname]);
+        }
+
+        public function testFirstNameFormat()
+        {
+            $fakeInfo = new FakeInfo();
+            $result = $fakeInfo->getFullNameAndGender();
+
+            $pattern = '/^[A-Za-zÆØÅæøå]+ [A-Z]\.$/';
+
+            $this->assertMatchesRegularExpression(
+                $pattern,
+                $result['firstName']
+            );
+        }
+
+        public function testLastNameFormat()
+        {
+            $fakeInfo = new FakeInfo();
+            $result = $fakeInfo->getFullNameAndGender();
+
+            $pattern = '/^[A-Za-zÆØÅæøå]+$/';
+
+            $this->assertMatchesRegularExpression(
+                $pattern,
+                $result['lastName']
+            );
+        }
+
+        public function testFirstNameNoNumbers()
+        {
+            $fakeInfo = new FakeInfo();
+            $result = $fakeInfo->getFullNameAndGender();
+
+            $this->assertDoesNotMatchRegularExpression('/[0-9]/', $result['firstName']);
+        }
     }
-
-    public function testFirstNameIsString()
-{
-    $fakeInfo = new FakeInfo();
-    $result = $fakeInfo->getFullNameAndGender();
-
-    $this->assertIsString($result['firstName']);
-}
-
-    public function testFirstNameNotEmpty()
-{
-    $fakeInfo = new FakeInfo();
-    $result = $fakeInfo->getFullNameAndGender();
-
-    $this->assertNotEmpty($result['firstName']);
-}
-
-public function testFirstNameFormat()
-{
-    $fakeInfo = new FakeInfo();
-    $result = $fakeInfo->getFullNameAndGender();
-
-    $pattern = '/^[A-Za-zÆØÅæøå]+ [A-Z]\.$/';
-
-    $this->assertMatchesRegularExpression(
-        $pattern,
-        $result['firstName']
-    );
-}
-
-public function testFirstNameNoNumbers()
-{
-    $fakeInfo = new FakeInfo();
-    $result = $fakeInfo->getFullNameAndGender();
-
-    $this->assertDoesNotMatchRegularExpression('/[0-9]/', $result['firstName']);
-}
-
-
-}
